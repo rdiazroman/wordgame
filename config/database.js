55ee43738082f3ecd60d513f17b34d4987694791
Ruben <rubendiazroman@Rubens-MBP.fritz.box>
2016-06-21 19:36:49 +0200

    Bug fixing

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
 module.exports = {
-    localUrl: 'mongodb://localhost/wordgamedb'
+    localUrl: 'mongodb://localhost/wordgamedb',
+    prodUrl: 'mongodb://ds049558.mlab.com:49558/heroku_pt3530fg
 };
